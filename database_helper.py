0d628f59eebe17fdbb26031fda8983b95aaca8cd
zainaliofficialtrada343-cloud <zainaliofficialtrada343@gmail.com>
2026-03-04 16:57:56 +0500

    Add files via upload

--- database_helper.py
+++ database_helper.py
@@ -7,7 +7,6 @@ conn = st.connection("gsheets", type=GSheetsConnection)
 
 def get_full_data():
     try:
-        # Sheet1 se patient records
         return conn.read(worksheet="Sheet1", ttl=0).dropna(how="all")
     except:
         cols = ["ID", "Date", "Name", "Age", "Gender", "Test", "Total_Bill", "Paid_Amount", "Remaining", "Result", "Unit", "Status"]
@@ -15,20 +14,23 @@ def get_full_data():
 
 def get_tests_list():
     try:
-        # Sheet2 se tests ki list - Yahan se dropdown banta hai
+        # Sheet2 se dropdown list uthana
         return conn.read(worksheet="Sheet2", ttl=0).dropna(how="all")
     except:
         return pd.DataFrame(columns=["Test_Name", "Rate"])
 
 def save_record_online(new_row_df):
     existing_data = get_full_data()
+    # Yahan 'updated_data' naam hai
     updated_data = pd.concat([existing_data, new_row_df], ignore_index=True)
+    # Neechay bhi 'updated_data' hi hona chahiye
     conn.update(data=updated_data, worksheet="Sheet1")
     st.cache_data.clear()
 
 def save_test_online(new_test_df):
     existing_tests = get_tests_list()
-    # Variable ka naam sahi hona chahiye: updated_tests
+    # Yahan 'updated_tests' naam hai
     updated_tests = pd.concat([existing_tests, new_test_df], ignore_index=True)
+    # Neechay bhi 'updated_tests' hi hona chahiye
     conn.update(data=updated_tests, worksheet="Sheet2")
     st.cache_data.clear()
